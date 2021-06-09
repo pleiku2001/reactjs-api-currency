fd11bb0fc37b82a341149f2e45def5275a0c0073
Nguyen Trung Kien <kient2005@gmail.com>
2021-06-09 12:23:46 +0700

    <commit_message>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState,useEffect } from "react";
+import Coin from "./Coin";
+const axios = require("axios");
 function App() {
+  const [search, setSearch] = useState("");
+  const [coins, setCoins] = useState([]);
+
+  const handleChange = (e) => setSearch(e.target.value);
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+        setCoins(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
+  const handleCoin=coins.filter(coin=>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App Get API Currency</h1>
+      <form action="">
+        <input type="text" onChange={handleChange} value={search} placeholder='type coin' className='input' />
+      </form>
+
+      <h3> List Coin </h3>
+      {handleCoin.map((e)=>{
+        return(
+          <Coin
+          key={e.id}
+          name={e.name}
+          symbol={e.symbol}
+          current_price={e.current_price}
+          image={e.image}
+          total_volume={e.total_volume}
+          />
+        )
+      })}
     </div>
   );
 }
